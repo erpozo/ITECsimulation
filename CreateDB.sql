b906998a93d60b134e957a1127859d7ce4368381
erpozo <pozo.erp.a@gmail.com>
2022-06-01 21:49:31 +0200

    Desarrollo de idea

---
+++ CreateDB.sql
@@ -0,0 +1,20 @@
+create database itec;
+
+create table alumnos (
+	alumnos_ID INT AUTO_INCREMENT PRIMARY KEY,
+    DNI CHAR(9) NOT NULL UNIQUE
+    Nombre varchat(20),
+    Apellido1 varchat(20),
+    Apellido2 varchat(20),
+);
+
+create table profesor (
+	profesor_ID INT AUTO_INCREMENT PRIMARY KEY,
+    DNI CHAR(9) NOT NULL UNIQUE
+    Nombre varchat(20)
+);
+
+create table asignatura (
+	asignatura_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre varchat(20)
+);
